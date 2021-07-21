c52275b0f79e4c18046d241bb6e2f5daa98f18ce
apoorvHearth <apoorv@heyhearth.com>
2021-07-21 11:37:28 +0530

    Feat: Add social link panel and assets

---
+++ components/SocialPanel/index.tsx
@@ -0,0 +1,52 @@
+import React from 'react'
+import styles from './styles.module.css'
+import YoutubeIcon from '../../assets/svgs/social-icons/youtube.svg'
+import FacebookIcon from '../../assets/svgs/social-icons/facebook.svg'
+import TwitterIcon from '../../assets/svgs/social-icons/twitter.svg'
+import Instagramicon from '../../assets/svgs/social-icons/instagram.svg'
+import { useThemeContext } from '../../components/ThemeProvider'
+
+export interface SocialItem {
+  icon: SvgrComponent,
+  url: string,
+  label?: string
+}
+
+interface Props {
+  items: SocialItem[],
+  backgroundColor: string,
+  iconColor: string
+}
+
+export const defaultItems: SocialItem[] = [
+  {
+    icon: TwitterIcon,
+    url: ''
+  },
+  {
+    icon: YoutubeIcon,
+    url: ''
+  },
+  {
+    icon: FacebookIcon,
+    url: ''
+  },
+  {
+    icon: Instagramicon,
+    url: ''
+  },
+]
+
+export const SocialPanel: React.FC<Props> = ({ items = defaultItems, iconColor = '#000', backgroundColor }) => {
+  const { colors } = useThemeContext()
+  const bgColor = backgroundColor || colors.primary
+  return (
+    <div className={styles.container}>
+      {items && items.map(item =>
+        <a className={styles.item} href={item.url} target="_blank" style={{ backgroundColor: bgColor }}>
+          <item.icon style={{ fill: iconColor }} />
+        </a>
+      )}
+    </div>
+  )
+}
--- next-env.d.ts
+++ next-env.d.ts
@@ -1,3 +1,11 @@
 /// <reference types="next" />
 /// <reference types="next/types/global" />
 /// <reference types="next/image-types/global" />
+
+
+interface SvgrComponent extends React.StatelessComponent<React.SVGAttributes<SVGElement>> { }
+
+declare module '*.svg' {
+  const value: SvgrComponent;
+  export default value;
+}
