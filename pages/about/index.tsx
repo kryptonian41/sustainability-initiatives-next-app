7f33e3297be735b4d48d8325bc7c9fb35af7e3a2
Abhishek Khatri <abhishekkhatri98@gmail.com>
2021-08-26 22:22:22 +0530

    feat: people from cms on about us page

--- pages/about/index.tsx
+++ pages/about/index.tsx
@@ -1,21 +1,45 @@
 import React from "react";
 import Wrapper from "components/Wrapper";
 import Hero from "./components/Hero";
+import { getStakeHolders } from "utils/api/client-side-api";
 import Promise from "./components/Promise";
 import PhotoGrid from "components/PhotoGrid";
 import { peopleProps } from "components/PhotoGrid/mockProps";
+import { StakeHolder } from "utils/types";
+import { GridItemProps } from "components/PhotoGrid/GridPhoto";
 
-const About = () => {
+interface Props {
+  stakeHolders: StakeHolder[];
+}
+
+export const getServerSideProps = async () => {
+  const stakeHolders = await getStakeHolders();
+  return {
+    props: {
+      stakeHolders,
+    },
+  };
+};
+
+const About: React.FC<Props> = ({ stakeHolders }) => {
+  const gridProps = (): GridItemProps[] =>
+    stakeHolders.map((stakeHolder) => ({
+      imgSrc: stakeHolder.photo.url,
+      title: stakeHolder.name,
+      subTitle: stakeHolder.designation,
+    }));
   return (
     <>
       <Hero />
       <Promise />
-      <div className="py-20">
-        <PhotoGrid
-          heading="The People who make it possible"
-          items={peopleProps}
-        />
-      </div>
+      {stakeHolders.length > 0 && (
+        <div className="py-20">
+          <PhotoGrid
+            heading="The People who make it possible"
+            items={gridProps()}
+          />
+        </div>
+      )}
     </>
   );
 };
