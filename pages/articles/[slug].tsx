0216a84b2ecac2a0e81a423819e2a87bcf6916c6
apoorvHearth <apoorv@heyhearth.com>
2021-12-07 23:23:55 +0530

    Fix: Bug with ISR config

--- pages/articles/[slug].tsx
+++ pages/articles/[slug].tsx
@@ -1,73 +1,73 @@
-import clsx from "clsx";
-import { Container } from "components/Container";
-import { SocialPanelIcon } from "components/SocialPanel";
-import { useThemeContext } from "components/ThemeProvider";
-import { GetStaticPaths, GetStaticProps } from "next";
-import React, { useMemo } from "react";
-import { FacebookShareButton, TwitterShareButton } from "react-share";
-import sanitizeHtml from "sanitize-html";
-import { getArticleBySlug, getArticles } from "utils/api/client-side-api";
-import { prettyDate } from "utils/helpers";
-import { useDeviceMediaQuery } from "utils/hooks/useMediaQuery";
-import { Article } from "utils/types";
-import FacebookIcon from "../../assets/svgs/social-icons/facebook.svg";
-import TwitterIcon from "../../assets/svgs/social-icons/twitter.svg";
-import styles from "./styles.module.css";
-import Slider from "react-slick";
-import { NextArrow, PrevArrow } from "components/SlideShow/arrows";
-import { SlideShow } from "components/SlideShow";
+import clsx from 'clsx'
+import { Container } from 'components/Container'
+import { SocialPanelIcon } from 'components/SocialPanel'
+import { useThemeContext } from 'components/ThemeProvider'
+import { GetStaticPaths, GetStaticProps } from 'next'
+import React, { useMemo } from 'react'
+import { FacebookShareButton, TwitterShareButton } from 'react-share'
+import sanitizeHtml from 'sanitize-html'
+import { getArticleBySlug, getArticles } from 'utils/api/client-side-api'
+import { prettyDate } from 'utils/helpers'
+import { useDeviceMediaQuery } from 'utils/hooks/useMediaQuery'
+import { Article } from 'utils/types'
+import FacebookIcon from '../../assets/svgs/social-icons/facebook.svg'
+import TwitterIcon from '../../assets/svgs/social-icons/twitter.svg'
+import styles from './styles.module.css'
+import Slider from 'react-slick'
+import { NextArrow, PrevArrow } from 'components/SlideShow/arrows'
+import { SlideShow } from 'components/SlideShow'
 
 interface Props {
-  article: Article;
+  article: Article
 }
 
-// export const getStaticPaths: GetStaticPaths = async () => {
-//   const articles = await getArticles();
-//   const paths = articles
-//     .slice(0, Math.min(10, articles.length))
-//     .map((article) => `/articles/${article.slug}`);
-//   return {
-//     paths,
-//     fallback: false,
-//   };
-// };
+export const getStaticPaths: GetStaticPaths = async () => {
+  const articles = await getArticles()
+  const paths = articles
+    .slice(0, Math.min(10, articles.length))
+    .map((article) => `/articles/${article.slug}`)
+  return {
+    paths,
+    fallback: 'blocking',
+  }
+}
 
-// export const getStaticProps: GetStaticProps<Props> = async ({ params }) => {
+export const getStaticProps: GetStaticProps<Props> = async ({ params }) => {
+  const { slug } = params
+  return {
+    props: {
+      article: await getArticleBySlug(slug as string),
+    },
+    revalidate: 60000,
+  }
+}
+
+// export const getServerSideProps = async ({ params }) => {
 //   const { slug } = params;
 //   return {
 //     props: {
 //       article: await getArticleBySlug(slug as string),
 //     },
-//     revalidate: 60000,
 //   };
 // };
-
-export const getServerSideProps = async ({ params }) => {
-  const { slug } = params;
-  return {
-    props: {
-      article: await getArticleBySlug(slug as string),
-    },
-  };
-};
 
 const BlogPage: React.FC<Props> = ({ article }) => {
   const articleDate = useMemo(
     () =>
-      `${prettyDate(
+      `${prettyDate(article.published_date, 'd MMMM')} '${prettyDate(
         article.published_date,
-        "d MMMM"
-      )} '${prettyDate(article.published_date, "yy")}`,
+        'yy'
+      )}`,
     [article]
-  );
+  )
   const images = useMemo(() => {
-    return article.images.map((image) => image.url);
-  }, [article]);
-  const { colors } = useThemeContext();
-  const matchesLaptop = useDeviceMediaQuery("laptop");
+    return article.images.map((image) => image.url)
+  }, [article])
+  const { colors } = useThemeContext()
+  const matchesLaptop = useDeviceMediaQuery('laptop')
   const articleUrl = useMemo(() => {
-    return typeof window === "undefined" ? "" : window.location.href;
-  }, []);
+    return typeof window === 'undefined' ? '' : window.location.href
+  }, [])
   return (
     <div className="py-20">
       {article?.heroImage && (
@@ -82,16 +82,18 @@ const BlogPage: React.FC<Props> = ({ article }) => {
       <Container className="flex flex-wrap">
         <div
           className={clsx({
-            "w-1/5 mr-10": matchesLaptop,
-            "w-full": !matchesLaptop,
+            'w-1/5 mr-10': matchesLaptop,
+            'w-full': !matchesLaptop,
           })}
         >
-          <p className={styles.date}>{article.published_date ? articleDate : ""}</p>
+          <p className={styles.date}>
+            {article.published_date ? articleDate : ''}
+          </p>
           <hr className="mt-3" />
           <div
             className={clsx({
-              "mt-10": matchesLaptop,
-              "mt-6": !matchesLaptop,
+              'mt-10': matchesLaptop,
+              'mt-6': !matchesLaptop,
             })}
           >
             <p className="mb-6">Share:</p>
@@ -122,19 +124,19 @@ const BlogPage: React.FC<Props> = ({ article }) => {
           </div>
         </div>
         <div
-          className={clsx("flex-1", {
-            "mt-0": matchesLaptop,
-            "mt-12": !matchesLaptop,
+          className={clsx('flex-1', {
+            'mt-0': matchesLaptop,
+            'mt-12': !matchesLaptop,
           })}
         >
-          <h3 className={clsx("text-2xl mb-10", styles.title)}>
+          <h3 className={clsx('text-2xl mb-10', styles.title)}>
             {article.title}
           </h3>
           <div
-            className={clsx("body", styles.articleBody)}
+            className={clsx('body', styles.articleBody)}
             dangerouslySetInnerHTML={{
               __html: sanitizeHtml(article.body, {
-                allowedTags: sanitizeHtml.defaults.allowedTags.concat(["img"]),
+                allowedTags: sanitizeHtml.defaults.allowedTags.concat(['img']),
               }),
             }}
           ></div>
@@ -144,7 +146,7 @@ const BlogPage: React.FC<Props> = ({ article }) => {
         </div>
       </Container>
     </div>
-  );
-};
+  )
+}
 
-export default BlogPage;
+export default BlogPage
